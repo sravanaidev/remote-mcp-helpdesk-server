9101c5e9a62abb7ec2ecf87562e63b7c1a80d2e8
sravan <sravan.aidev@gmail.com>
2026-07-23 22:50:21 -0500

    edited main

--- main.py
+++ main.py
@@ -129,5 +129,5 @@ def estimate_resolution_time(priority: str) -> str:
 
 
 if __name__ == "__main__":
-    #mcp.run(transport="http", host="0.0.0.0", port=8080)
-    mcp.run()  # for local use
+    mcp.run(transport="http", host="0.0.0.0", port=8080)
+    #mcp.run()  # for local use
